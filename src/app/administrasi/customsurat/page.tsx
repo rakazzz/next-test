654b0814fb7419f389f4c8df240ce6a3168f710e
rakazzz <raka.barakatuh40@gmail.com>
2023-08-02 13:18:25 +0800

    afg

--- src/app/administrasi/customsurat/page.tsx
+++ src/app/administrasi/customsurat/page.tsx
@@ -1,9 +1,8 @@
-import UploadImg from '../../components/imgUpload'
+
 export default function Page() {
     return (
         <div>
             ini custom surat
-            <UploadImg/>
-        </div>
+                    </div>
     )
 }
